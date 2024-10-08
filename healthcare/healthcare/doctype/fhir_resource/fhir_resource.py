779a4a7c9cc289f66ac0a24c396bc15ded1fcac1
Sajin SR <sajin@earthianslive.com>
2024-10-08 14:45:34 +0530

    fix: Support child table map in FHIR Resource

--- healthcare/healthcare/doctype/fhir_resource/fhir_resource.py
+++ healthcare/healthcare/doctype/fhir_resource/fhir_resource.py
@@ -2,6 +2,7 @@
 # # For license information, please see license.txt
 
 import json
+from copy import deepcopy
 
 import frappe
 from frappe.model.document import Document
@@ -19,14 +20,38 @@ def make_fhir_resource(doc, method=None):
 	mapper_exists = frappe.db.exists(
 		"FHIR Resource Mapper", {"document_type": doc.doctype, "version": settings.fhir_version}
 	)
-	resource_data = {}
-	if mapper_exists:
-		mapper_doc = frappe.get_doc("FHIR Resource Mapper", mapper_exists)
-		for i in mapper_doc.data:
-			if i.resource_field:
-				resource_data[i.resource_field] = doc.get(i.doc_field)
 
-	if resource_data:
+	if not mapper_exists:
+		return
+
+	mapper_doc = frappe.get_doc("FHIR Resource Mapper", mapper_exists)
+	resource_type_json = frappe.db.get_value(
+		"Resource Type", mapper_doc.resource_type, "resource_json"
+	)
+
+	if isinstance(resource_type_json, str):
+		resource_type_json = json.loads(resource_type_json)
+
+	for resource in resource_type_json:
+		if isinstance(resource_type_json[resource], list):
+			child_rows = [d for d in mapper_doc.resource_maps if d.get("parent_resource_field") == resource]
+			child_docfield = child_rows[0].get("parent_docfield") if len(child_rows) else None
+
+			child_resource = []
+			if child_docfield:
+				for item in doc.get(child_docfield):
+					child_json = {}
+					for child in resource_type_json[resource][0]:
+						child_json = deepcopy(
+							make_resource_map(item, child, resource_type_json[resource][0], child_rows)
+						)
+					child_resource.append(child_json)
+
+			resource_type_json[resource] = child_resource
+		else:
+			make_resource_map(doc, resource, resource_type_json, mapper_doc.resource_maps)
+
+	if resource_type_json:
 		resource_exists = frappe.db.exists(
 			"FHIR Resource", {"reference_doctype": doc.doctype, "referance_doc_name": doc.name}
 		)
@@ -34,8 +59,19 @@ def make_fhir_resource(doc, method=None):
 			resource = frappe.new_doc("FHIR Resource")
 			resource.reference_doctype = doc.doctype
 			resource.referance_doc_name = doc.name
-			resource.resource_data = json.dumps(resource_data, indent=2, sort_keys=True, default=str)
+			resource.resource_data = json.dumps(resource_type_json, indent=2, default=str)
 		else:
 			resource = frappe.get_doc("FHIR Resource", resource_exists)
-			resource.resource_data = json.dumps(resource_data, indent=2, sort_keys=True, default=str)
+			resource.resource_data = json.dumps(resource_type_json, indent=2, default=str)
+
 		resource.save(ignore_permissions=True)
+
+
+def make_resource_map(doc, resource, json, mapper_doc_child):
+	json[resource] = None
+
+	for d in mapper_doc_child:
+		if d.get("resource_field") == resource:
+			json[resource] = doc.get(d.doc_field)
+
+	return json
--- healthcare/healthcare/doctype/fhir_resource_mapper/fhir_resource_mapper.js
+++ healthcare/healthcare/doctype/fhir_resource_mapper/fhir_resource_mapper.js
@@ -1,21 +1,41 @@
 // // Copyright (c) 2024, earthians Health Informatics Pvt. Ltd. and contributors
 // // For license information, please see license.txt
 
+var common_docfields = ["doctype", "name"];
+var fields_excluded = ["Section Break", "Column Break", "Tab Break", "HTML"];
+
 frappe.ui.form.on("FHIR Resource Mapper", {
 	document_type: function (frm) {
 		if (frm.doc.document_type) {
-			frappe.model.with_doctype(frm.doc.document_type, function () {
-				let meta = frappe.get_meta(frm.doc.document_type);
-				let fields = meta.fields;
+			frm.clear_table("resource_maps");
+			frappe.model.with_doctype(frm.doc.document_type, () => {
+				common_docfields.forEach(function (field) {
+					frm.add_child("resource_maps", {
+						doc_field: field,
+					});
+				});
 
-				fields.forEach(field => {
-					if (!["Section Break", "Column Break", "Tab Break"].includes(field.fieldtype)) {
-						frm.add_child("data", {
-							doc_field: field.fieldname,
-						});
+				frappe.get_meta(frm.doc.document_type).fields.forEach(function (d) {
+					if (!fields_excluded.includes(d.fieldtype)) {
+						if (["Table", "Table MultiSelect"].includes(d.fieldtype)) {
+							frappe.model.with_doctype(d.options, () => {
+								frappe.get_meta(d.options).fields.forEach(function (child) {
+									if (!fields_excluded.includes(child.fieldtype)) {
+										frm.add_child("resource_maps", {
+											doc_field: child.fieldname,
+											parent_docfield: d.fieldname,
+										});
+									}
+								});
+							});
+						} else {
+							frm.add_child("resource_maps", {
+								doc_field: d.fieldname,
+							});
+						}
 					}
 				});
-				frm.refresh("data");
+				frm.refresh_field("resource_maps");
 			});
 		}
 	},
