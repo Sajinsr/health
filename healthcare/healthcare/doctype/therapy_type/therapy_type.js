fb38a77fc8f5b724500a624c0e27ecf80f1e0579
Sajin SR <sajin@earthianslive.com>
2025-03-15 20:59:07 +0530

    fix(Therapy Type): quick entry type error - remove setup event and use in_list_view

--- healthcare/healthcare/doctype/therapy_type/therapy_type.js
+++ healthcare/healthcare/doctype/therapy_type/therapy_type.js
@@ -2,15 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Therapy Type', {
-	setup: function(frm) {
-		frm.get_field('exercises').grid.editable_fields = [
-			{fieldname: 'exercise_type', columns: 7},
-			{fieldname: 'difficulty_level', columns: 1},
-			{fieldname: 'counts_target', columns: 1},
-			{fieldname: 'assistance_level', columns: 1}
-		];
-	},
-
 	refresh: function(frm) {
 		if (!frm.doc.__islocal) {
 			cur_frm.add_custom_button(__('Change Item Code'), function() {
