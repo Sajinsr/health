earthians/marley

rename doctypes Healthcare Service Order > Service Request Healthcare Service Order Category > Service Request Category Healthcare Service Order Priority > Service Request Priority Healthcare Service Order Intent > Service Request Intent Healthcare Service Order Reason > Service Request Reason

healthcare/patches/v0_0/setup_service_request_and_insurance.py
@@ -1,15 +1,15 @@
 from __future__ import unicode_literals
 import frappe
-from healthcare.healthcare.setup import setup_healthcare_service_order_masters, create_customer_groups
+from healthcare.healthcare.setup import setup_service_request_masters, create_customer_groups
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 from healthcare.healthcare.healthcare import data
 
 def execute():
 	frappe.reload_doc('healthcare', 'doctype', 'Patient Care Type')
-	frappe.reload_doc('healthcare', 'doctype', 'Healthcare Service Order Intent')
-	frappe.reload_doc('healthcare', 'doctype', 'Healthcare Service Order Priority')
+	frappe.reload_doc('healthcare', 'doctype', 'Service Request Intent')
+	frappe.reload_doc('healthcare', 'doctype', 'Service Request Priority')
 
-	setup_healthcare_service_order_masters()
+	setup_service_request_masters()
 	create_customer_groups()
 
 	frappe.reload_doc('accounts', 'doctype', 'sales_invoice')

Sajinsr/health

healthcare/patches/v0_0/setup_service_request_and_insurance.py
@@ -4,18 +4,15 @@ import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 from healthcare.healthcare.healthcare import data
-from healthcare.healthcare.setup import (
-	create_customer_groups,
-	setup_healthcare_service_order_masters,
-)
+from healthcare.healthcare.setup import create_customer_groups, setup_service_request_masters
 
 
 def execute():
 	frappe.reload_doc("healthcare", "doctype", "Patient Care Type")
-	frappe.reload_doc("healthcare", "doctype", "Healthcare Service Order Intent")
-	frappe.reload_doc("healthcare", "doctype", "Healthcare Service Order Priority")
+	frappe.reload_doc("healthcare", "doctype", "Service Request Intent")
+	frappe.reload_doc("healthcare", "doctype", "Service Request Priority")
 
-	setup_healthcare_service_order_masters()
+	setup_service_request_masters()
 	create_customer_groups()
 
 	frappe.reload_doc("accounts", "doctype", "sales_invoice")